cad80ec320587adaf6299245681c27c9cd72d2fa
rallagas <rallagas@bicol-u.edu.ph>
2025-01-22 09:19:44 +0800

    hide attachments and fixed merchant error

--- client/_shop.php
+++ client/_shop.php
@@ -322,12 +322,12 @@ if (!empty($_SESSION['user_profile'])) {
                                 <div class="modal-footer">
                                     <div class="d-inline-block AlertStatus"></div>
 
-                                    <input type="file" id="additionalFileAttachment" class="visually-hidden">
+                                    <!-- <input type="file" id="additionalFileAttachment" class="visually-hidden">
                                     <button class="float-start btn btn-light"> Attach Files
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-paperclip" viewBox="0 0 16 16">
                                         <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0z"/>
                                         </svg>
-                                    </button>
+                                    </button> -->
                                     <button id="placeOrderBtn" type="submit" class="btn btn-primary"
                                         data-bs-target="#PaymentModal" data-bs-toggle="modal">Place Order</button>
                                 </div>
